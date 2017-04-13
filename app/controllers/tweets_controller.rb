c34140479786ef1b9e2cb51a0b41c2b660cb0197
Ken Murata <k_murata@nttpc.co.jp>
2017-04-13 23:45:52 +0000

    DIVE02 done. next is DIVE 03 confirm

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+    before_action :set_blog, only: [:edit, :update, :destroy]
+    
     def index
         @tweets = Tweet.all
     end
@@ -8,12 +10,36 @@ class TweetsController < ApplicationController
     end
     
     def create
-        Tweet.create(tweets_params)
-        redirect_to tweets_path
+        @tweet = Tweet.new(tweets_params)
+        if @tweet.save
+            redirect_to tweets_path, notice: "つぶやきました!"
+        else
+            render 'new'
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @tweet.update(tweets_params)
+            redirect_to tweets_path, notice: "編集しました!"
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @tweet.destroy
+        redirect_to tweets_path, notice: "削除完了!"
     end
     
     private
         def tweets_params
             params.require(:tweet).permit(:content)
+        end
+        
+        def set_blog
+            @tweet = Tweet.find(params[:id])
         end
 end
